4eb466120d845abc42a3fb161922e044d268752a
abcyds <abcyds@tmon.co.kr>
2022-12-06 22:54:44 +0900

    no message

---
+++ inflean-the-java-test/src/main/java/com/ydskingdom/infleanthejavatest/InfleanTheJavaTestApplication.java
@@ -0,0 +1,13 @@
+package com.ydskingdom.infleanthejavatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfleanTheJavaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfleanTheJavaTestApplication.class, args);
+    }
+
+}
---
+++ inflean-the-java-test/src/test/java/com/ydskingdom/infleanthejavatest/InfleanTheJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydskingdom.infleanthejavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfleanTheJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
